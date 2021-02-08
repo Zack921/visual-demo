c835a8309c258f46c9422b8221934982555f1961
zackguo <zackguo@tencent.com>
2021-02-08 18:32:48 +0800

    feat(webgl): 基础彩色立方体

---
+++ webgl/coloredCube/index.js
@@ -0,0 +1,132 @@
+const VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  attribute vec4 a_Color;
+  uniform mat4 u_MvpMatrix; // 投影*视图*模型
+  varying vec4 v_Color;
+  void main() {
+    gl_Position = u_MvpMatrix * a_Position;
+    v_Color = a_Color;
+  }
+`;
+
+const FSHADER_SOURCE = `
+  precision mediump float;
+  varying vec4 v_Color;
+  void main() {
+    gl_FragColor = v_Color;
+  }
+`;
+
+function main() {
+  const canvas = document.getElementById('webgl');
+
+  const gl = getWebGLContext(canvas);
+  if (!gl) {
+    return console.log('Failed to get the rendering context for WebGL');
+  }
+
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    return console.log('Failed to intialize shaders.');
+  }
+
+  const n = initVertexBuffers(gl);
+  if (n < 0) {
+    return console.log('Failed to set the vertex information');
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST); // 开启深度检测-消除被遮挡住的表面
+
+  const u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  if (!u_MvpMatrix) {
+    return console.log('Failed to get the storage location of u_MvpMatrix');
+  }
+
+  const mvpMatrix = new Matrix4();
+  mvpMatrix.setPerspective(30, 1, 1, 100); // 正交投影
+  mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0); // 视图矩阵
+
+  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // 清除颜色缓冲区和深度缓冲区
+
+  // 根据索引绘制
+  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+}
+
+function initVertexBuffers(gl) {
+  // Create a cube
+  //    v6----- v5
+  //   /|      /|
+  //  v1------v0|
+  //  | |     | |
+  //  | |v7---|-|v4
+  //  |/      |/
+  //  v2------v3
+
+  const vertices = new Float32Array([   // Vertex coordinates
+     1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+     1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+     1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+    -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+    -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+     1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+  ]);
+
+  const colors = new Float32Array([     // Colors
+    0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+    0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+    1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+    1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+    1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+    0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+  ]);
+
+  const indices = new Uint8Array([       // Indices of the vertices
+     0, 1, 2,   0, 2, 3,    // front
+     4, 5, 6,   4, 6, 7,    // right
+     8, 9,10,   8,10,11,    // up
+    12,13,14,  12,14,15,    // left
+    16,17,18,  16,18,19,    // down
+    20,21,22,  20,22,23     // back
+  ]);
+
+  const indexBuffer = gl.createBuffer();
+  if (!indexBuffer) 
+    return -1;
+
+  if (!initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position'))
+    return -1;
+
+  if (!initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color'))
+    return -1;
+
+  // 写入顶点的索引数据
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return indices.length;
+}
+
+function initArrayBuffer(gl, data, num, type, attribute) {
+  const buffer = gl.createBuffer();
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  const a_attribute = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    console.log('Failed to get the storage location of ' + attribute);
+    return false;
+  }
+  
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+
+  gl.enableVertexAttribArray(a_attribute);
+
+  return true;
+}
