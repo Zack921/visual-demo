d7537d182264aa40945285758a0a51775a5ad2bc
zackguo <zackguo@tencent.com>
2021-02-09 20:23:28 +0800

    feat(webgl): 利用阴影贴图实现阴影

---
+++ webgl/examples/shadow/index.js
@@ -0,0 +1,328 @@
+// 生成阴影纹理
+const SHADOW_VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  uniform mat4 u_MvpMatrix;
+  void main() {
+    gl_Position = u_MvpMatrix * a_Position;
+  }
+`;
+
+const SHADOW_FSHADER_SOURCE = `
+  precision mediump float;
+  void main() {
+    gl_FragColor = vec4(gl_FragCoord.z, 0.0, 0.0, 0.0); // 将片元的z值写入色值
+  }
+`;
+
+// 绘制场景
+const VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  attribute vec4 a_Color;
+  uniform mat4 u_MvpMatrix;
+  uniform mat4 u_MvpMatrixFromLight;
+  varying vec4 v_PositionFromLight;
+  varying vec4 v_Color;
+  void main() {
+    gl_Position = u_MvpMatrix * a_Position;
+    v_PositionFromLight = u_MvpMatrixFromLight * a_Position; // 以光源为视点的坐标
+    v_Color = a_Color;
+  }
+`;
+
+const FSHADER_SOURCE = `
+  precision mediump float;
+  uniform sampler2D u_ShadowMap;
+  varying vec4 v_PositionFromLight;
+  varying vec4 v_Color;
+  void main() {
+    vec3 shadowCoord = (v_PositionFromLight.xyz/v_PositionFromLight.w)/2.0 + 0.5; // 归一化到[-1,1]区间
+    vec4 rgbaDepth = texture2D(u_ShadowMap, shadowCoord.xy);
+    float depth = rgbaDepth.r; // 阴影纹理上该点的z值
+    float visibility = (shadowCoord.z > depth + 0.005) ? 0.7 : 1.0; // +0.005以消除马赫带，主要是精度影响。shadowCoord.z是16位，depth是8位
+    gl_FragColor = vec4(v_Color.rgb * visibility, v_Color.a);
+  }
+`;
+
+const OFFSCREEN_WIDTH = 2048, OFFSCREEN_HEIGHT = 2048; // 离屏绘制的尺寸
+const LIGHT_X = 0, LIGHT_Y = 7, LIGHT_Z = 2; // 光源坐标
+
+function main() {
+  const canvas = document.getElementById('webgl');
+
+  const gl = getWebGLContext(canvas);
+  if (!gl) {
+    return console.log('Failed to get the rendering context for WebGL');
+  }
+
+  const shadowProgram = createProgram(gl, SHADOW_VSHADER_SOURCE, SHADOW_FSHADER_SOURCE);
+  shadowProgram.a_Position = gl.getAttribLocation(shadowProgram, 'a_Position');
+  shadowProgram.u_MvpMatrix = gl.getUniformLocation(shadowProgram, 'u_MvpMatrix');
+  if (shadowProgram.a_Position < 0 || !shadowProgram.u_MvpMatrix) {
+    return console.log('Failed to get the storage location of attribute or uniform variable from shadowProgram'); 
+  }
+
+  const normalProgram = createProgram(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+  normalProgram.a_Position = gl.getAttribLocation(normalProgram, 'a_Position');
+  normalProgram.a_Color = gl.getAttribLocation(normalProgram, 'a_Color');
+  normalProgram.u_MvpMatrix = gl.getUniformLocation(normalProgram, 'u_MvpMatrix');
+  normalProgram.u_MvpMatrixFromLight = gl.getUniformLocation(normalProgram, 'u_MvpMatrixFromLight');
+  normalProgram.u_ShadowMap = gl.getUniformLocation(normalProgram, 'u_ShadowMap');
+  if (normalProgram.a_Position < 0 || normalProgram.a_Color < 0 || !normalProgram.u_MvpMatrix ||
+      !normalProgram.u_MvpMatrixFromLight || !normalProgram.u_ShadowMap) {
+    return console.log('Failed to get the storage location of attribute or uniform variable from normalProgram'); 
+  }
+
+  const triangle = initVertexBuffersForTriangle(gl);
+  const plane = initVertexBuffersForPlane(gl);
+  if (!triangle || !plane) {
+    return console.log('Failed to set the vertex information');
+  }
+
+  const fbo = initFramebufferObject(gl);
+  if (!fbo) {
+    return console.log('Failed to initialize frame buffer object');
+  }
+
+  gl.clearColor(0, 0, 0, 1);
+  gl.enable(gl.DEPTH_TEST);
+
+  // 变换为以光源作为观察点
+  const viewProjMatrixFromLight = new Matrix4();
+  viewProjMatrixFromLight.setPerspective(70.0, OFFSCREEN_WIDTH/OFFSCREEN_HEIGHT, 1.0, 100.0);
+  viewProjMatrixFromLight.lookAt(LIGHT_X, LIGHT_Y, LIGHT_Z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+  const viewProjMatrix = new Matrix4();
+  viewProjMatrix.setPerspective(45, canvas.width/canvas.height, 1.0, 100.0);
+  viewProjMatrix.lookAt(0.0, 7.0, 9.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+  let currentAngle = 0.0;
+  const mvpMatrixFromLight_t = new Matrix4();
+  const mvpMatrixFromLight_p = new Matrix4();
+  const tick = function() {
+    currentAngle = animate(currentAngle);
+
+    // 把绘制目标切换到帧缓冲区，拿到阴影纹理
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    gl.viewport(0, 0, OFFSCREEN_HEIGHT, OFFSCREEN_HEIGHT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
+
+    gl.useProgram(shadowProgram);
+
+    drawTriangle(gl, shadowProgram, triangle, currentAngle, viewProjMatrixFromLight);
+    mvpMatrixFromLight_t.set(g_mvpMatrix);
+    drawPlane(gl, shadowProgram, plane, viewProjMatrixFromLight);
+    mvpMatrixFromLight_p.set(g_mvpMatrix);
+
+    // 把绘制目标切换到canvas
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, fbo.texture);
+
+    gl.useProgram(normalProgram);
+    gl.uniform1i(normalProgram.u_ShadowMap, 0);
+
+    gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
+    drawTriangle(gl, normalProgram, triangle, currentAngle, viewProjMatrix);
+    gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_p.elements);
+    drawPlane(gl, normalProgram, plane, viewProjMatrix);
+
+    window.requestAnimationFrame(tick, canvas);
+  };
+  tick(); 
+}
+
+const g_modelMatrix = new Matrix4();
+const g_mvpMatrix = new Matrix4();
+function drawTriangle(gl, program, triangle, angle, viewProjMatrix) {
+  g_modelMatrix.setRotate(angle, 0, 1, 0);
+  draw(gl, program, triangle, viewProjMatrix);
+}
+
+function drawPlane(gl, program, plane, viewProjMatrix) {
+  g_modelMatrix.setRotate(-45, 0, 1, 1);
+  draw(gl, program, plane, viewProjMatrix);
+}
+
+function draw(gl, program, o, viewProjMatrix) {
+  initAttributeVariable(gl, program.a_Position, o.vertexBuffer);
+  if (program.a_Color != undefined)
+    initAttributeVariable(gl, program.a_Color, o.colorBuffer);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, o.indexBuffer);
+
+  g_mvpMatrix.set(viewProjMatrix);
+  g_mvpMatrix.multiply(g_modelMatrix);
+  gl.uniformMatrix4fv(program.u_MvpMatrix, false, g_mvpMatrix.elements);
+
+  gl.drawElements(gl.TRIANGLES, o.numIndices, gl.UNSIGNED_BYTE, 0);
+}
+
+function initAttributeVariable(gl, a_attribute, buffer) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(a_attribute, buffer.num, buffer.type, false, 0, 0);
+  gl.enableVertexAttribArray(a_attribute);
+}
+
+function initVertexBuffersForPlane(gl) {
+  // Create a plane
+  //  v1------v0
+  //  |        | 
+  //  |        |
+  //  |        |
+  //  v2------v3
+
+  // Vertex coordinates
+  const vertices = new Float32Array([
+    3.0, -1.7, 2.5,  -3.0, -1.7, 2.5,  -3.0, -1.7, -2.5,   3.0, -1.7, -2.5    // v0-v1-v2-v3
+  ]);
+
+  // Colors
+  const colors = new Float32Array([
+    1.0, 1.0, 1.0,    1.0, 1.0, 1.0,  1.0, 1.0, 1.0,   1.0, 1.0, 1.0
+  ]);
+
+  // Indices of the vertices
+  const indices = new Uint8Array([0, 1, 2,   0, 2, 3]);
+
+  const o = new Object();
+
+  o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+  o.colorBuffer = initArrayBufferForLaterUse(gl, colors, 3, gl.FLOAT);
+  o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+  if (!o.vertexBuffer || !o.colorBuffer || !o.indexBuffer) return null; 
+
+  o.numIndices = indices.length;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return o;
+}
+
+function initVertexBuffersForTriangle(gl) {
+  // Create a triangle
+  //       v2
+  //      / | 
+  //     /  |
+  //    /   |
+  //  v0----v1
+
+  // Vertex coordinates
+  const vertices = new Float32Array([-0.8, 3.5, 0.0,  0.8, 3.5, 0.0,  0.0, 3.5, 1.8]);
+  // Colors
+  const colors = new Float32Array([1.0, 0.5, 0.0,  1.0, 0.5, 0.0,  1.0, 0.0, 0.0]);    
+  // Indices of the vertices
+  const indices = new Uint8Array([0, 1, 2]);
+
+  const o = new Object();
+
+  o.vertexBuffer = initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT);
+  o.colorBuffer = initArrayBufferForLaterUse(gl, colors, 3, gl.FLOAT);
+  o.indexBuffer = initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE);
+  if (!o.vertexBuffer || !o.colorBuffer || !o.indexBuffer) return null; 
+
+  o.numIndices = indices.length;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return o;
+}
+
+function initArrayBufferForLaterUse(gl, data, num, type) {
+  const buffer = gl.createBuffer();
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return null;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  buffer.num = num;
+  buffer.type = type;
+
+  return buffer;
+}
+
+function initElementArrayBufferForLaterUse(gl, data, type) {
+  const buffer = gl.createBuffer();
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return null;
+  }
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  buffer.type = type;
+
+  return buffer;
+}
+
+function initFramebufferObject(gl) {
+  let framebuffer, texture, depthBuffer;
+
+  const error = function() {
+    if (framebuffer) gl.deleteFramebuffer(framebuffer);
+    if (texture) gl.deleteTexture(texture);
+    if (depthBuffer) gl.deleteRenderbuffer(depthBuffer);
+    return null;
+  }
+
+  framebuffer = gl.createFramebuffer(); // 帧缓冲区
+  if (!framebuffer) {
+    console.log('Failed to create frame buffer object');
+    return error();
+  }
+
+  texture = gl.createTexture();
+  if (!texture) {
+    console.log('Failed to create texture object');
+    return error();
+  }
+  gl.bindTexture(gl.TEXTURE_2D, texture); // 绑定 纹理对象 到 纹理单元 上
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT, 0, gl.RGBA, gl.UNSIGNED_BYTE, null); // 清空 纹理对象 上的纹理数据
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR); // 配置纹理对象参数，设置纹理图像映射到图形上的方式
+
+  depthBuffer = gl.createRenderbuffer(); // 渲染缓冲区
+  if (!depthBuffer) {
+    console.log('Failed to create renderbuffer object');
+    return error();
+  }
+  gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer); // 绑定渲染缓冲区对象
+  gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT); // 设置尺寸
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0); // 设置帧缓冲区上的颜色关联对象为纹理对象
+  gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer); // 设置帧缓冲区上的深度关联对象为渲染缓冲区对象
+
+  const e = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
+  if (gl.FRAMEBUFFER_COMPLETE !== e) {
+    console.log('Frame buffer object is incomplete: ' + e.toString());
+    return error();
+  }
+
+  framebuffer.texture = texture;
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+
+  return framebuffer;
+}
+
+const ANGLE_STEP = 40;
+
+let last = Date.now();
+function animate(angle) {
+  const now = Date.now();
+  const elapsed = now - last;
+  last = now;
+
+  const newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+  return newAngle % 360;
+}
