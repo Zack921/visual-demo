b097fe40c3d0029709555637dd4c301ea8f4da14
zackguo <zackguo@tencent.com>
2021-04-26 16:39:04 +0800

    feat: 阴影成功出来了，后面再优化下代码

--- threejs/diyShadow/index.js
+++ threejs/diyShadow/index.js
@@ -28,7 +28,7 @@ function initThree() {
 
   renderer.setSize(width, height);
 
-  renderer.setClearColor("rgb(255,255,247)", 1.0);
+  renderer.setClearColor("rgb(150,150,150)", 1.0);
 }
 
 function initScene() {
@@ -58,17 +58,17 @@ function initCamera() {
   camera.lookAt(scene.position);
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
+  scene.add(camera);
 
-  camera2 = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  // camera2 = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  camera2 = new THREE.OrthographicCamera( -100, 100, 100, -100, 1, 70 );
   camera2.position.x = 20;
   camera2.position.y = 50;
   camera2.position.z = 0;
-  camera2.lookAt(scene.position);
+  camera2.lookAt(bufferScene.position);
   camera2.aspect = width / height;
   camera2.updateProjectionMatrix();
   bufferScene.add(camera2);
-
-  scene.add(camera);
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.update();
@@ -95,33 +95,41 @@ function initLight() {
   const lightCube2 = new THREE.Mesh(lightGeo, lightMat);
   lightCube2.position.x = 20;
   lightCube2.position.y = 50;
-  bufferScene.add(lightCube2);
+  // bufferScene.add(lightCube2);
 }
 
 function initObject() {
-  const geometry = new THREE.PlaneGeometry(80, 80, 32);
-  const material = new THREE.MeshPhongMaterial({
-    color: 0xffff00,
-    side: THREE.DoubleSide,
+  const smMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      projectionMatrixSM: { value: camera2.projectionMatrix },
+    },
+    vertexShader: document.getElementById("vertexShaderSM").textContent,
+    fragmentShader: document.getElementById("fragmentShaderSM").textContent,
   });
+  console.log('camera2.modelViewMatrix: ', camera2.modelViewMatrix);
+  console.log('camera2.projectionMatrixSM: ', camera2.projectionMatrix);
+
+  const geometry = new THREE.BoxGeometry(40, 40, 1);
   var planeMaterial = new THREE.MeshBasicMaterial({
-    map: bufferTexture.depthTexture, //获取渲染目标缓冲区中的纹理
+    map: bufferTexture.texture, //获取渲染目标缓冲区中的纹理
   });
   const plane = new THREE.Mesh(geometry, planeMaterial);
+  plane.position.x = -50;
+  plane.position.y = 30;
   // plane.rotation.x = Math.PI / 2;
   scene.add(plane);
 
-  const plane2 = new THREE.Mesh(geometry, material);
+  const plane2 = new THREE.Mesh(geometry, smMaterial);
   plane2.rotation.x = Math.PI / 2;
-  // bufferScene.add(plane2);
+  bufferScene.add(plane2);
 
   const cubeGeo = new THREE.BoxGeometry(20, 20, 20);
   const cubeMat = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(cubeGeo, cubeMat);
+  const cube = new THREE.Mesh(cubeGeo, smMaterial);
+  // const cube = new THREE.Mesh(cubeGeo, cubeMat);
   cube.position.y += 10;
-  // scene.add(cube);
+  cube.name = 'cube';
   bufferScene.add(cube);
-  console.log("bufferScene: ", bufferScene);
 
   const diyMaterial = new THREE.ShaderMaterial({
     // attributes 居然传不进去
@@ -129,9 +137,20 @@ function initObject() {
     //   test: { value: 0.5 },
     // },
     uniforms: {
-      modelViewMatrixSM: { value: camera2.modelViewMatrix },
+      modelViewMatrixSM: { value: cube.modelViewMatrix },
       projectionMatrixSM: { value: camera2.projectionMatrix },
-      depthTexture: { value: bufferTexture.depthTexture },
+      depthTexture: { value: bufferTexture.texture },
+    },
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+  });
+  console.log('modelViewMatrixSM: ', cube.modelViewMatrix);
+  console.log('projectionMatrixSM: ', camera2.projectionMatrix);
+  const diyMaterial2 = new THREE.ShaderMaterial({
+    uniforms: {
+      modelViewMatrixSM: { value: plane2.modelViewMatrix },
+      projectionMatrixSM: { value: camera2.projectionMatrix },
+      depthTexture: { value: bufferTexture.texture },
     },
     vertexShader: document.getElementById("vertexShader").textContent,
     fragmentShader: document.getElementById("fragmentShader").textContent,
@@ -142,6 +161,13 @@ function initObject() {
   var mesh = new THREE.Mesh(bGeometry, diyMaterial); // 网格模型对象Mesh
   mesh.position.y += 10;
   scene.add(mesh);
+
+  const plane3 = new THREE.Mesh(geometry, diyMaterial2);
+  plane3.rotation.x = Math.PI / 2;
+  scene.add(plane3);
+
+  console.log("bufferScene: ", bufferScene);
+  console.log('scene: ', scene);
 }
 
 function render() {
