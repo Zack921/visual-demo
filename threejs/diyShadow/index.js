37f1025e746fd78eb92561104291e0d443483105
zackguo <zackguo@tencent.com>
2021-04-27 15:47:04 +0800

    feat: 保存优化后的版本

--- threejs/diyShadow/index.js
+++ threejs/diyShadow/index.js
@@ -3,9 +3,8 @@ import { OrbitControls } from "./assets/orbitcontrols.js";
 let renderer;
 let stats;
 let camera;
-let camera2;
+let camera4SM;
 let scene;
-let light;
 let bufferScene;
 let bufferTexture;
 
@@ -28,7 +27,7 @@ function initThree() {
 
   renderer.setSize(width, height);
 
-  renderer.setClearColor("rgb(150,150,150)", 1.0);
+  renderer.setClearColor("rgb(255,255,255)", 1.0);
 }
 
 function initScene() {
@@ -52,23 +51,19 @@ function initCamera() {
   const height = domElement.clientHeight;
 
   camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  camera.position.x = 50;
-  camera.position.y = 50;
-  camera.position.z = 200;
+  camera.position.set(50, 50, 200);
   camera.lookAt(scene.position);
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
   scene.add(camera);
 
-  // camera2 = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-  camera2 = new THREE.OrthographicCamera( -100, 100, 100, -100, 1, 70 );
-  camera2.position.x = 20;
-  camera2.position.y = 50;
-  camera2.position.z = 0;
-  camera2.lookAt(bufferScene.position);
-  camera2.aspect = width / height;
-  camera2.updateProjectionMatrix();
-  bufferScene.add(camera2);
+  // camera2 = new THREE.PerspectiveCamera(45, width / height, 1, 10000); 暂时不知道为啥不能使用透视相机
+  camera4SM = new THREE.OrthographicCamera(-100, 100, 100, -100, 1, 70);
+  camera4SM.position.set(20, 50, 0);
+  camera4SM.lookAt(bufferScene.position);
+  camera4SM.aspect = width / height;
+  camera4SM.updateProjectionMatrix();
+  bufferScene.add(camera4SM);
 
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.update();
@@ -81,110 +76,94 @@ function initLight() {
   const light = new THREE.DirectionalLight(0xffffff); // 平行光
   light.position.set(20, 50, 0);
   scene.add(light);
-  const light2 = new THREE.DirectionalLight(0xffffff); // 平行光
-  light2.position.set(20, 50, 0);
-  bufferScene.add(light2);
 
   const lightGeo = new THREE.BoxGeometry(5, 5, 5);
   const lightMat = new THREE.MeshBasicMaterial({ color: 0x00ffff });
   const lightCube = new THREE.Mesh(lightGeo, lightMat);
-  lightCube.position.x = 20;
-  lightCube.position.y = 50;
+  lightCube.position.set(20, 50, 0);
   scene.add(lightCube);
-
-  const lightCube2 = new THREE.Mesh(lightGeo, lightMat);
-  lightCube2.position.x = 20;
-  lightCube2.position.y = 50;
-  // bufferScene.add(lightCube2);
 }
 
 function initObject() {
-  const smMaterial = new THREE.ShaderMaterial({
+  // add object in buffer scene
+  const getSMMaterial = new THREE.ShaderMaterial({
     uniforms: {
-      projectionMatrixSM: { value: camera2.projectionMatrix },
+      projectionMatrixSM: { value: camera4SM.projectionMatrix },
     },
     vertexShader: document.getElementById("vertexShaderSM").textContent,
     fragmentShader: document.getElementById("fragmentShaderSM").textContent,
   });
-  console.log('camera2.modelViewMatrix: ', camera2.modelViewMatrix);
-  console.log('camera2.projectionMatrixSM: ', camera2.projectionMatrix);
 
-  const geometry = new THREE.BoxGeometry(40, 40, 1);
-  var planeMaterial = new THREE.MeshBasicMaterial({
-    map: bufferTexture.texture, //获取渲染目标缓冲区中的纹理
-  });
-  const plane = new THREE.Mesh(geometry, planeMaterial);
-  plane.position.x = -50;
-  plane.position.y = 30;
-  // plane.rotation.x = Math.PI / 2;
-  scene.add(plane);
-
-  const plane2 = new THREE.Mesh(geometry, smMaterial);
-  plane2.rotation.x = Math.PI / 2;
-  bufferScene.add(plane2);
+  const groundGeo = new THREE.BoxGeometry(40, 40, 1);
+  const groundInBuffer = new THREE.Mesh(groundGeo, getSMMaterial);
+  groundInBuffer.rotation.x = Math.PI / 2;
+  groundInBuffer.name = "groundPlane";
+  bufferScene.add(groundInBuffer);
 
   const cubeGeo = new THREE.BoxGeometry(20, 20, 20);
-  const cubeMat = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(cubeGeo, smMaterial);
-  // const cube = new THREE.Mesh(cubeGeo, cubeMat);
-  cube.position.y += 10;
-  cube.name = 'cube';
-  bufferScene.add(cube);
+  const cubeInBuffer = new THREE.Mesh(cubeGeo, getSMMaterial);
+  cubeInBuffer.position.y += 10;
+  cubeInBuffer.name = "cubeInBuffer";
+  bufferScene.add(cubeInBuffer);
 
-  const diyMaterial = new THREE.ShaderMaterial({
+  // add object in screen scene
+  const useSM4CubeMat = new THREE.ShaderMaterial({
     // attributes 居然传不进去
-    // attributes: {
-    //   test: { value: 0.5 },
-    // },
     uniforms: {
-      modelViewMatrixSM: { value: cube.modelViewMatrix },
-      projectionMatrixSM: { value: camera2.projectionMatrix },
+      modelViewMatrixSM: { value: cubeInBuffer.modelViewMatrix },
+      projectionMatrixSM: { value: camera4SM.projectionMatrix },
       depthTexture: { value: bufferTexture.texture },
+      color: { value: new THREE.Vector3(0, 1, 0) },
     },
     vertexShader: document.getElementById("vertexShader").textContent,
     fragmentShader: document.getElementById("fragmentShader").textContent,
   });
-  console.log('modelViewMatrixSM: ', cube.modelViewMatrix);
-  console.log('projectionMatrixSM: ', camera2.projectionMatrix);
-  const diyMaterial2 = new THREE.ShaderMaterial({
-    uniforms: {
-      modelViewMatrixSM: { value: plane2.modelViewMatrix },
-      projectionMatrixSM: { value: camera2.projectionMatrix },
-      depthTexture: { value: bufferTexture.texture },
-    },
-    vertexShader: document.getElementById("vertexShader").textContent,
-    fragmentShader: document.getElementById("fragmentShader").textContent,
-  });
-  const cubeGeo2 = new THREE.BoxGeometry(20, 20, 20);
-  const bGeometry = new THREE.BufferGeometry();
-  bGeometry.fromGeometry(cubeGeo2);
-  var mesh = new THREE.Mesh(bGeometry, diyMaterial); // 网格模型对象Mesh
-  mesh.position.y += 10;
-  scene.add(mesh);
 
-  const plane3 = new THREE.Mesh(geometry, diyMaterial2);
-  plane3.rotation.x = Math.PI / 2;
-  scene.add(plane3);
+  const cubeBufGeo = new THREE.BufferGeometry();
+  cubeBufGeo.fromGeometry(cubeGeo);
+  const cubeInScreen = new THREE.Mesh(cubeBufGeo, useSM4CubeMat);
+  cubeInScreen.position.y += 10;
+  cubeInScreen.name = "cubeInScreen";
+  scene.add(cubeInScreen);
+
+  const useSM4GroundMat = new THREE.ShaderMaterial({
+    uniforms: {
+      modelViewMatrixSM: { value: groundInBuffer.modelViewMatrix },
+      projectionMatrixSM: { value: camera4SM.projectionMatrix },
+      depthTexture: { value: bufferTexture.texture },
+      color: { value: new THREE.Vector3(1, 1, 1) },
+    },
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+  });
+
+  const planeInScreen = new THREE.Mesh(groundGeo, useSM4GroundMat);
+  planeInScreen.rotation.x = Math.PI / 2;
+  planeInScreen.name = "planeInScreen";
+  scene.add(planeInScreen);
+
+  // 展示 shadow map
+  const showSMGeo = new THREE.BoxGeometry(40, 40, 1);
+  const smMaterial = new THREE.MeshBasicMaterial({
+    map: bufferTexture.texture, //获取渲染目标缓冲区中的纹理
+  });
+  const showSMInScreen = new THREE.Mesh(showSMGeo, smMaterial);
+  showSMInScreen.position.set(-50, 30, 0);
+  showSMInScreen.name = "showSMInScreen";
+  scene.add(showSMInScreen);
 
   console.log("bufferScene: ", bufferScene);
-  console.log('scene: ', scene);
+  console.log("scene: ", scene);
 }
 
 function render() {
   // 渲染到目标缓冲区
-  // camera.position.x = 20;
-  // camera.position.y = 50;
-  // camera.position.z = 0;
-  // camera.lookAt(bufferScene.position);
-  // camera.updateProjectionMatrix();
+  renderer.setClearColor("rgb(255,255,255)", 1.0);
   renderer.setRenderTarget(bufferTexture);
-  renderer.render(bufferScene, camera2);
+  renderer.render(bufferScene, camera4SM);
+
   // 渲染到屏幕
-  // camera.position.x = 50;
-  // camera.position.y = 50;
-  // camera.position.z = 200;
-  // camera.lookAt(scene.position);
-  // camera.updateProjectionMatrix();
+  renderer.setClearColor("rgb(150,150,150)", 1.0);
   renderer.setRenderTarget(null);
   renderer.render(scene, camera);
 
